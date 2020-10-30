ece68fea08a23360f2ede169df7f2cd156c15e5b
Marco <mrvs2500@gmail.com>
2020-10-30 11:02:23 -0700

    String Methods

--- Strings/stringMethods.py
+++ Strings/stringMethods.py
@@ -11,3 +11,10 @@ print(spam.islower())
 
 print(spam.isupper())
 
+# string formatting 
+name = 'Alice'
+place = 'Main Street'
+time = '6 pm'
+food = 'turnips'
+
+print('Hello %s, you are invited to a party at %s at %s. Please bring %s.' %(name, place, time, food))
