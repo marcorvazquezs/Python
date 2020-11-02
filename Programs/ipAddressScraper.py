0eddf55bf6cc8c29aa7de0e7dd452204ed7d4153
Marco <mrvs2500@gmail.com>
2020-11-02 10:13:33 -0800

    Script to scrape IP Addresses

--- Programs/ipAddressScraper.py
+++ Programs/ipAddressScraper.py
@@ -6,13 +6,13 @@ import re, pyperclip
 # regex to match IP Addresses
 ipRegex = re.compile(r'''
 (
-(\d){1,3}     # first octet
-.             # first period 
-(\d){1,3}     # second octet 
-.             # second period
-(\d){1,3}     # third octet 
-.             # third period 
-(\d){1,3} # fourth octet
+\d{1,3}       # first octet
+\.             # first period 
+\d{1,3}       # second octet 
+\.             # second period
+\d{1,3}       # third octet 
+\.             # third period 
+\d{1,3}       # fourth octet
 )
 ''', re.VERBOSE)
 
@@ -22,16 +22,17 @@ text = pyperclip.paste()
 # search matching IP address pattern
 extractedIPs = ipRegex.findall(text)
 
-# extract full IP address from result tuples
-allIPs = []
 count = 0 # variable to count number of resulting IP addresses
 
 for ipAddress in extractedIPs: 
-    allIPs.append(ipAddress[0])
     count+=1
+
 # cleanup results 
-results = '\n'.join(allIPs)
+results = '\n'.join(extractedIPs)
 
 # print results
-print('The following ' + str(count) + ' IP Addresses were found:')
-print(results)
+if count >= 1:
+    print('The following ' + str(count) + ' IP Addresses were found:\n')
+    print(results)
+else:
+    print('No IP Addresses were found.')
