4a401c57fb35da9e90ab25a0627ca91f70cc6d65
Marco <mrvs2500@gmail.com>
2020-11-05 11:40:15 -0800

    Web Scraping

--- Web_Scraping/webScraping.py
+++ Web_Scraping/webScraping.py
@@ -2,16 +2,18 @@
 
 import bs4, requests
 
-def getAmazonPrice(productURL):
+def getAmazonPrice(productUrl):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
     }
-    res = requests.get(productURL, headers=headers)
+    res = requests.get(productUrl, headers=headers)
     res.raise_for_status()
 
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    elems = soup.select('#newOfferAccordionRow .header-price')
+    elems = soup.select('#mediaNoAccordion > div.a-row > div.a-column.a-span4.a-text-right.a-span-last > span.a-size-medium.a-color-price.header-price')
+    
     return elems[0].text.strip()
 
 price = getAmazonPrice('http://www.amazon.com/Automate-Boring-Stuff-Python-Programming/dp/1593275994/ref=tmm_pap_swatch_0?_encoding=UTF8&amp;qid=&amp;sr=')
 print('The price is ' + price)
+
